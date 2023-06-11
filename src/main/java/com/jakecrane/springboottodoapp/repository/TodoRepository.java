2d48f7379ead5ba72c75a65f0fd4b620083016bf
Jake Crane <jakecrane91@gmail.com>
2023-06-11 18:06:06 -0500

    Remove unused import

--- src/main/java/com/jakecrane/springboottodoapp/repository/TodoRepository.java
+++ src/main/java/com/jakecrane/springboottodoapp/repository/TodoRepository.java
@@ -3,8 +3,6 @@ package com.jakecrane.springboottodoapp.repository;
 import com.jakecrane.springboottodoapp.entity.Todo;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-
 public interface TodoRepository extends CrudRepository<Todo, Long> {
 
 }
